ee8b5012a2b020ac103ed4d0b7d247722de669c4
Dadaş <Dada@DESKTOP-BQT9OT7>
2022-09-06 18:28:03 +0300

    Format (Validation) kontrolü sağlandı.

---
+++ TelefonRehberi/Migrations/20220906151642_FormatDuzenlemesi.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TelefonRehberi.Migrations
+{
+    public partial class FormatDuzenlemesi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon_Numarasi",
+                table: "TelefonRehberleri",
+                type: "varchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Fax_Numarasi",
+                table: "TelefonRehberleri",
+                type: "varchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon_Numarasi",
+                table: "TelefonRehberleri",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Fax_Numarasi",
+                table: "TelefonRehberleri",
+                type: "varchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(11)",
+                oldMaxLength: 11);
+        }
+    }
+}
--- TelefonRehberi/Models/Rehber.cs
+++ TelefonRehberi/Models/Rehber.cs
@@ -26,16 +26,23 @@ namespace TelefonRehberi.Models
         [Column(TypeName = "varchar(20)")]
         [DisplayName("Telefon Numarası")]
         [Required(ErrorMessage = "Bu Alanı Doldurmak Zorunludur.")]
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(11, ErrorMessage = "Telefon numarası 11 haneli olmalıdır"), MinLength(11, ErrorMessage = "Telefon numarası 11 haneli olmalıdır")]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Telefon numarası format dışı")]
         public string Telefon_Numarasi { get; set; }
 
         [Column(TypeName = "varchar(50)")]
         [DisplayName("Fax Numarası")]
         [Required(ErrorMessage = "Bu Alanı Doldurmak Zorunludur.")]
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(11, ErrorMessage = "Fax numarası 11 haneli olmalıdır"), MinLength(11, ErrorMessage = "Fax numarası 11 haneli olmalıdır")]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Telefon numarası format dışı")]
         public string Fax_Numarasi { get; set; }
 
         [Column(TypeName = "varchar(50)")]
         [DisplayName("E-Mail")]
         [Required(ErrorMessage = "Bu Alanı Doldurmak Zorunludur.")]
+        [EmailAddress(ErrorMessage = "Lütfen Düzgün Email Formatı Giriniz.")]
         public string E_Mail { get; set; }
 
 
